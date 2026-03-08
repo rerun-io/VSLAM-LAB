343a9b89020ac2a5c88349586180b6eb4dac2308
alejandrofontan <fontanvillacampa@gmail.com>
2026-03-08 10:16:26 +1000

    pep8

--- Datasets/dataset_files/dataset_hilti2026.py
+++ Datasets/dataset_files/dataset_hilti2026.py
@@ -13,7 +13,7 @@ from Datasets.DatasetVSLAMLab import DatasetVSLAMLab
 
 SCRIPT_LABEL = f"\033[95m[{os.path.basename(__file__)}]\033[0m "
 
-ROSBAG_NAME = f"rosbag.db3"
+ROSBAG_NAME = "rosbag.db3"
 IMU_TOPIC = "/imu/data_raw"
 IMAGE_TOPIC = "/image_raw/compressed"
 CALIBRATION_FILE = "kalibr_imucam_chain.yaml"
@@ -43,10 +43,10 @@ class HILTI2026_dataset(DatasetVSLAMLab):
         if not calibration_file.exists():
             folder_id = "1kYxgaCAtsVLe1B1MGsc2kR6RnByloHUV"
 
-            file_url = f"https://drive.google.com/file/d/1MX_C9kphWyghcQKNN-x70SquzepN588k/view?usp=sharing"
+            file_url = "https://drive.google.com/file/d/1MX_C9kphWyghcQKNN-x70SquzepN588k/view?usp=sharing"
             gdown.download(
                 url=file_url,
-                output=str(self.dataset_path /  "kalibr_imucam_chain.yaml"),
+                output=str(self.dataset_path / "kalibr_imucam_chain.yaml"),
                 quiet=False,
                 use_cookies=False,
                 resume=True,
@@ -90,7 +90,9 @@ class HILTI2026_dataset(DatasetVSLAMLab):
             if rgb_path.exists():
                 continue
             rgb_path.mkdir(parents=True, exist_ok=True)
-            command = f"pixi run -e ros2 extract-ros2bag-frames --rosbag_path {rosbag} --sequence_path {sequence_path} --image_topic {image_topic} --cam {cam}"
+
+            inputs = f"--rosbag_path {rosbag} --sequence_path {sequence_path} --image_topic {image_topic} --cam {cam}"
+            command = f"pixi run -e ros2 extract-ros2bag-frames {inputs}"
             subprocess.run(command, shell=True)
 
     def create_rgb_csv(self, sequence_name: str) -> None:
@@ -102,7 +104,9 @@ class HILTI2026_dataset(DatasetVSLAMLab):
         imu_csv = sequence_path / "imu_0.csv"
         if imu_csv.exists():
             return
-        command = f"pixi run -e ros2 extract-ros2bag-imu --rosbag_path {rosbag} --sequence_path {sequence_path} --imu_topic {IMU_TOPIC}"
+
+        inputs = f"--rosbag_path {rosbag} --sequence_path {sequence_path} --imu_topic {IMU_TOPIC}"
+        command = f"pixi run -e ros2 extract-ros2bag-imu {inputs}"
         subprocess.run(command, shell=True)
 
         rgb_csv = sequence_path / "rgb.csv"
@@ -136,29 +140,33 @@ class HILTI2026_dataset(DatasetVSLAMLab):
 
         cam0 = data["cam0"]
         cam1 = data["cam1"]
-
+        intrinsics0 = cam0["intrinsics"]
+        intrinsics1 = cam1["intrinsics"]
         T_cam0_imu = np.array(cam0["T_cam_imu"], dtype=float).reshape(4, 4)
         T_cam1_imu = np.array(cam1["T_cam_imu"], dtype=float).reshape(4, 4)
 
-        rgb0: dict[str, Any] = {"cam_name": "rgb_0", "cam_type": "gray",
-                "cam_model": "pinhole", "focal_length": cam0["intrinsics"][0:2], "principal_point": cam0["intrinsics"][2:4],
-                "distortion_type": "equid4", "distortion_coefficients": cam0["distortion_coeffs"],
-                "fps": self.rgb_hz,
-                "T_BS": np.linalg.inv(T_cam0_imu)}
+        rgb0: dict[str, Any] = {
+            "cam_name": "rgb_0", "cam_type": "gray",
+            "cam_model": "pinhole", "focal_length": intrinsics0[0:2], "principal_point": intrinsics0[2:4],
+            "distortion_type": "equid4", "distortion_coefficients": cam0["distortion_coeffs"],
+            "fps": self.rgb_hz,
+            "T_BS": np.linalg.inv(T_cam0_imu)}
 
-        rgb1: dict[str, Any] = {"cam_name": "rgb_1", "cam_type": "gray",
-                "cam_model": "pinhole", "focal_length": cam1["intrinsics"][0:2], "principal_point": cam1["intrinsics"][2:4],
-                "distortion_type": "equid4", "distortion_coefficients": cam1["distortion_coeffs"],
-                "fps": self.rgb_hz,
-                "T_BS": np.linalg.inv(T_cam1_imu)}
+        rgb1: dict[str, Any] = {
+             "cam_name": "rgb_1", "cam_type": "gray",
+             "cam_model": "pinhole", "focal_length": intrinsics1[0:2], "principal_point": intrinsics1[2:4],
+             "distortion_type": "equid4", "distortion_coefficients": cam1["distortion_coeffs"],
+             "fps": self.rgb_hz,
+             "T_BS": np.linalg.inv(T_cam1_imu)}
 
-        imu: dict[str, Any] = {"imu_name": "imu_0",
+        imu: dict[str, Any] = {
+            "imu_name": "imu_0",
             "a_max":  176.0, "g_max": 7.8,
             "sigma_g_c": 0.00047032046, "sigma_a_c": 0.00285891188,
             "sigma_bg": 0.0, "sigma_ba":  0.0,
             "sigma_gw_c":  0.00036433633, "sigma_aw_c": 0.00130039805,
-            "g":  9.81007, "g0": [ 0.0, 0.0, 0.0 ], "a0": [ 0.0, 0.0, 0.0 ],
-            "s_a":  [ 1.0,  1.0, 1.0 ],
+            "g":  9.81007, "g0": [0.0, 0.0, 0.0], "a0": [0.0, 0.0, 0.0],
+            "s_a":  [1.0,  1.0, 1.0],
             "fps": 1000.0,
             "T_BS": np.array(np.eye(4)).reshape((4, 4))}
 
@@ -166,12 +174,12 @@ class HILTI2026_dataset(DatasetVSLAMLab):
 
     def create_groundtruth_csv(self, sequence_name: str) -> None:
         sequence_path = self.dataset_path / sequence_name
-        groundtruth_txt = sequence_path / "groundtruth.txt"
+        gt_txt = sequence_path / "groundtruth.txt"
         groundtruth_csv = sequence_path / "groundtruth.csv"
         tmp = groundtruth_csv.with_suffix(".csv.tmp")
 
-        if groundtruth_txt.exists():
-            with open(groundtruth_txt, "r", encoding="utf-8") as fin, open(tmp, "w", encoding="utf-8", newline="") as fout:
+        if gt_txt.exists():
+            with open(gt_txt, "r", encoding="utf-8") as fin, open(tmp, "w", encoding="utf-8", newline="") as fout:
                 # Skip first lines (header/comments), then write CSV header + values
                 lines = fin.readlines()
                 data_lines = [ln.strip() for ln in lines[1:] if ln.strip() and not ln.lstrip().startswith("#")]
@@ -183,74 +191,74 @@ class HILTI2026_dataset(DatasetVSLAMLab):
                     new_line = f"{ts_ns}," + ",".join(parts[1:]) + "\n"
                     fout.write(new_line)
         else:
-             with open(tmp, "w", newline="", encoding="utf-8") as fout:
+            with open(tmp, "w", newline="", encoding="utf-8") as fout:
                 w = csv.writer(fout)
-                w.writerow(["ts (ns)","tx (m)","ty (m)","tz (m)","qx","qy","qz","qw"])
+                w.writerow(["ts (ns)", "tx (m)", "ty (m)", "tz (m)", "qx", "qy", "qz", "qw"])
 
         tmp.replace(groundtruth_csv)
         tmp.unlink(missing_ok=True)
 
     def _get_folder_id(self, sequence_name):
         if sequence_name == "floor_1_2025_05_05":
-           return "1ZiQHwcoF6NGo8lAW9kmqMgqMYQuQcxKZ"
+            return "1ZiQHwcoF6NGo8lAW9kmqMgqMYQuQcxKZ"
         if sequence_name == "floor_1_2025_07_07":
-           return "1yT3ISWjMLNPoF0LaH7iepgaJ_XKN-Npd"
+            return "1yT3ISWjMLNPoF0LaH7iepgaJ_XKN-Npd"
         if sequence_name == "floor_1_2025_12_02":
-           return "1OQ6t5SuEQ0txcAKS2hXJiZc7N3WTuXQi"
+            return "1OQ6t5SuEQ0txcAKS2hXJiZc7N3WTuXQi"
         if sequence_name == "floor_2_2025_05_05":
-           return "1JWYdtx7sU8YEGfNMQod_UcCc12nMAOWC"
+            return "1JWYdtx7sU8YEGfNMQod_UcCc12nMAOWC"
         if sequence_name == "floor_2_2025_10_28_run_1":
-           return "1xZbqsjl_ElmD_LHGJLeFCLyZGUOisU_E"
+            return "1xZbqsjl_ElmD_LHGJLeFCLyZGUOisU_E"
         if sequence_name == "floor_2_2025_10_28_run_2":
-           return "1ypN26UYznPYH_eag2cLvu48nzVQAc_ya"
+            return "1ypN26UYznPYH_eag2cLvu48nzVQAc_ya"
         if sequence_name == "floor_2_2025_12_02":
-           return "1kNC02bBxTzqVkKXCGsZcV86uKJEloV53"
+            return "1kNC02bBxTzqVkKXCGsZcV86uKJEloV53"
         if sequence_name == "floor_2_2025_12_03":
-           return "1OfyyCQS5fHSf7DTcMlMqyqZhrr_rJ2e4"
+            return "1OfyyCQS5fHSf7DTcMlMqyqZhrr_rJ2e4"
         if sequence_name == "floor_3_2025_05_19":
-           return "1S529WppSa7L5u9glioXuKuG6_hNJT99A"
+            return "1S529WppSa7L5u9glioXuKuG6_hNJT99A"
         if sequence_name == "floor_3_2025_12_02":
-           return "1gZyCZUcFK2DhuXD-yM6Zz5CqjjpC2Aqx"
+            return "1gZyCZUcFK2DhuXD-yM6Zz5CqjjpC2Aqx"
         if sequence_name == "floor_4_2025_05_19":
-           return "1kCBMvwRPiwDrWPi_z_B8CEZolDrfZvvy"
+            return "1kCBMvwRPiwDrWPi_z_B8CEZolDrfZvvy"
         if sequence_name == "floor_4_2025_12_02":
-           return "1Org1UoNa6h_OFNcmoom3M1vB6fwHKQs4"
+            return "1Org1UoNa6h_OFNcmoom3M1vB6fwHKQs4"
         if sequence_name == "floor_5_2025_12_02":
-           return "1AqnJ0VwLylxCL5fJpKAidvh21rhaPqlL"
+            return "1AqnJ0VwLylxCL5fJpKAidvh21rhaPqlL"
         if sequence_name == "floor_6_2025_06_18":
-           return "1k8FfHptZlNAf5aawmK-NJBSEyrZy9zwc"
+            return "1k8FfHptZlNAf5aawmK-NJBSEyrZy9zwc"
         if sequence_name == "floor_6_2025_07_07":
-           return "1rygrtU47OSFGPBSeyPSmWH9zrh-e3G08"
+            return "1rygrtU47OSFGPBSeyPSmWH9zrh-e3G08"
         if sequence_name == "floor_6_2025_12_02_run_1":
-           return "10ngMYxUxKuWrBQzJIorKPyLGvofzpTFX"
+            return "10ngMYxUxKuWrBQzJIorKPyLGvofzpTFX"
         if sequence_name == "floor_6_2025_12_02_run_2":
-           return "1CkU7--2PP0k58m1RRBD5PQygKGAiNQTn"
+            return "1CkU7--2PP0k58m1RRBD5PQygKGAiNQTn"
         if sequence_name == "floor_7_2025_12_02_run_1":
-           return "1PB-ILVt4pyoFrAjgvzfSosMepdv8af9V"
+            return "1PB-ILVt4pyoFrAjgvzfSosMepdv8af9V"
         if sequence_name == "floor_7_2025_12_02_run_2":
-           return "1aCebuVIX9-buMyb_ji4sCvwNo1rFhoLq"
+            return "1aCebuVIX9-buMyb_ji4sCvwNo1rFhoLq"
         if sequence_name == "floor_7_2025_12_03":
-           return "1MfhlsR5rB_1fVccTFGll3xozAsHgQamw"
+            return "1MfhlsR5rB_1fVccTFGll3xozAsHgQamw"
         if sequence_name == "floor_EG_2025_10_16":
-           return "1JOoZrrLmdpyOfvFsJANgYI66WZ8GGOoJ"
+            return "1JOoZrrLmdpyOfvFsJANgYI66WZ8GGOoJ"
         if sequence_name == "floor_EG_2025_12_02_run_1":
-           return "1S2c3u8C1vc9YgXtcIuXZuPLmMzVPHr2r"
+            return "1S2c3u8C1vc9YgXtcIuXZuPLmMzVPHr2r"
         if sequence_name == "floor_EG_2025_12_02_run_2":
-           return "159sCP0DbkOiNF8n83BhSFDUMW-jzaZFX"
+            return "159sCP0DbkOiNF8n83BhSFDUMW-jzaZFX"
         if sequence_name == "floor_UG1_2025_05_19":
-           return "1E5syGjNmzhQdvJ0F-92nkbWEsVfnxxm9"
+            return "1E5syGjNmzhQdvJ0F-92nkbWEsVfnxxm9"
         if sequence_name == "floor_UG1_2025_06_18":
-           return "1ZWrFeP0BPnXCOsctbRPc4C6oUfEqYYUZ"
+            return "1ZWrFeP0BPnXCOsctbRPc4C6oUfEqYYUZ"
         if sequence_name == "floor_UG1_2025_10_16":
-           return "1dSqJgKZZlhKdepl2_y1vxuf6_TbRMFHB"
+            return "1dSqJgKZZlhKdepl2_y1vxuf6_TbRMFHB"
         if sequence_name == "floor_UG1_2025_12_02_run_1":
-           return "14U2XwVP7z1mkeEgwpXEmrr1FTTojhFSt"
+            return "14U2XwVP7z1mkeEgwpXEmrr1FTTojhFSt"
         if sequence_name == "floor_UG1_2025_12_02_run_2":
-           return "1EHU2enVTHdfZ3I7SSahmqs-ad34odDZu"
+            return "1EHU2enVTHdfZ3I7SSahmqs-ad34odDZu"
         if sequence_name == "floor_UG1_2025_12_03":
-           return "1aUwNFRRs3u6KQgHxmQQGLbsKe_9xlvSM"
+            return "1aUwNFRRs3u6KQgHxmQQGLbsKe_9xlvSM"
         if sequence_name == "floor_UG2_2025_12_02":
-           return "1j_Uzae7lpmStVGUFZYV_ghQC_R5jF6nA"
+            return "1j_Uzae7lpmStVGUFZYV_ghQC_R5jF6nA"
 
     def _get_gt_url(self, sequence_name):
         if sequence_name == "floor_1_2025_05_05":
